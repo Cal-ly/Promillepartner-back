403b9f754cc61674c095cd923b5f9f2624e6e431
CharlineBennekou <149783592+CharlineBennekou@users.noreply.github.com>
2024-12-05 09:50:55 +0100

    Added new mockdata drinks and change dthe existing ones to have a realistic volume

--- PromillePartner BackEnd/Controllers/DrinkController.cs
+++ PromillePartner BackEnd/Controllers/DrinkController.cs
@@ -15,16 +15,27 @@ namespace PromillePartner_BackEnd.Controllers
         public ActionResult<IEnumerable<Drink>> Index()
         {
             var drinklist = new List<Drink>();
-            drinklist.Add(new Drink(1, "Whiskey", 0.7, 40.0, "Strong"));
-            drinklist.Add(new Drink(2, "Red Wine", 0.75, 13.5, "Wine"));
-            drinklist.Add(new Drink(3, "Beer", 0.5, 5.0, "Beer"));
-            drinklist.Add(new Drink(4, "Vodka", 0.7, 37.5, "Strong"));
-            drinklist.Add(new Drink(5, "White Wine", 0.75, 12.0, "Wine"));
-            drinklist.Add(new Drink(6, "Tequila", 0.7, 38.0, "Strong"));
-            drinklist.Add(new Drink(7, "Cider", 0.33, 4.5, "Light Alcohol"));
-            drinklist.Add(new Drink(8, "Gin", 0.7, 40.0, "Strong"));
-            drinklist.Add(new Drink(9, "Rum", 0.7, 37.5, "Strong"));
-            drinklist.Add(new Drink(10, "Champagne", 0.75, 12.0, "Wine"));
+            drinklist.Add(new Drink(7, "Cider", 0.33, 4.5, "Light Alcohol"));            // 11.72 grams
+            drinklist.Add(new Drink(3, "Beer", 0.33, 5.0, "Beer"));                      // 13.01 grams
+            drinklist.Add(new Drink(24, "Prosecco", 0.125, 11.0, "Wine"));               // 10.84 grams
+            drinklist.Add(new Drink(5, "White Wine", 0.15, 12.0, "Wine"));               // 14.19 grams
+            drinklist.Add(new Drink(2, "Red Wine", 0.15, 13.5, "Wine"));                 // 15.98 grams
+            drinklist.Add(new Drink(10, "Champagne", 0.125, 12.0, "Wine"));              // 11.84 grams
+            drinklist.Add(new Drink(15, "Sangria", 0.15, 12.0, "Wine"));                 // 14.19 grams
+            drinklist.Add(new Drink(12, "Pina Colada", 0.2, 13.0, "Cocktail"));          // 20.49 grams
+            drinklist.Add(new Drink(18, "Whiskey Sour", 0.2, 16.0, "Cocktail"));         // 25.25 grams
+            drinklist.Add(new Drink(13, "Margarita", 0.2, 15.0, "Cocktail"));            // 23.67 grams
+            drinklist.Add(new Drink(19, "Aperol Spritz", 0.25, 11.0, "Cocktail"));       // 21.71 grams
+            drinklist.Add(new Drink(25, "Jagerbomb", 0.2, 15.0, "Cocktail"));            // 23.67 grams
+            drinklist.Add(new Drink(11, "Mojito", 0.2, 10.0, "Cocktail"));               // 15.78 grams
+            drinklist.Add(new Drink(21, "Baileys", 0.05, 17.0, "Liqueur"));              // 6.71 grams
+            drinklist.Add(new Drink(8, "Gin Shot", 0.03, 40.0, "Strong"));               // 9.47 grams
+            drinklist.Add(new Drink(6, "Tequila Shot", 0.04, 38.0, "Strong"));           // 11.99 grams
+            drinklist.Add(new Drink(4, "Vodka Shot", 0.04, 37.5, "Strong"));             // 11.84 grams
+            drinklist.Add(new Drink(1, "Whiskey", 0.03, 40.0, "Strong"));                // 9.47 grams
+            drinklist.Add(new Drink(9, "Rum Shot", 0.03, 37.5, "Strong"));               // 8.86 grams
+            drinklist.Add(new Drink(14, "Long Island Iced Tea", 0.25, 22.0, "Cocktail"));// 43.34 grams
+            drinklist.Add(new Drink(20, "Espresso Martini", 0.2, 18.0, "Cocktail"));     // 28.40 grams
 
             return Ok(drinklist);
         }
